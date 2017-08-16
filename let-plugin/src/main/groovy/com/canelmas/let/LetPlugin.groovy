6e6b4cd6e00e082975953a1d99cfa953e154d239
Tyler Olson <tyler.olson@blinker.com>
2017-08-16 14:19:05 -0600

    upgrade stuff

--- let-plugin/src/main/groovy/com/canelmas/let/LetPlugin.groovy
+++ let-plugin/src/main/groovy/com/canelmas/let/LetPlugin.groovy
@@ -44,7 +44,7 @@ class LetPlugin implements Plugin<Project> {
 
         project.dependencies {
             compile 'com.github.blinkergit.let:let-runtime:0.1.14'
-            compile 'org.aspectj:aspectjrt:1.8.6'
+            compile 'org.aspectj:aspectjrt:1.8.10'
             compile 'com.github.blinkergit.let:let-annotations:0.1.14'
         }
 
